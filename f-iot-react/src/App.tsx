64da53400385750edf8fe7e37c3d58db39374031
ehdgn <ehdgn6570@gmail.com>
2025-10-29 17:45:08 +0900

    20251029 react

--- f-iot-react/src/App.tsx
+++ f-iot-react/src/App.tsx
@@ -2,12 +2,15 @@ import './App.css'
 import Basic from '@/pages/a_basic';
 
 function App() {
-
   return (
     <>
-      <h1>안녕하세요</h1>
+      <h1>안녕하세요 첫 리액트입니다 :)</h1>
       <Basic />
-    
+      {/* 
+      <div style={{ backgroundColor: 'orange' }}>
+        <Img />
+      </div> 
+      */}
     </>
   )
 }
