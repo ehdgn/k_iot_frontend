71a2c488ac580fd7f54e3a52b915668fa27f58c4
ehdgn <ehdgn6570@gmail.com>
2025-10-31 10:19:06 +0900

    20251031 route & link

--- f-iot-react/src/App.tsx
+++ f-iot-react/src/App.tsx
@@ -1,11 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Basic from '@/pages/a_basic';
+import RoutePages from '@/pages/b_route';
+import Navibar from './components/Navibar';
 
 function App() {
   return (
     <>
-      <h1>안녕하세요 첫 리액트입니다 :)</h1>
-      <Basic />
+      {/* 경로와 상관없이 렌더링  */}
+      <h1>Korea IoT React</h1>
+      <Navibar />
+
+      {/* Routes 태그: Route를 감싸는 컴포넌트 */}
+      <Routes>
+        {/* Route 태그: 특정 경로에 컴포넌트 지정 (단일 태그 권장) */}
+        <Route path='/basic' element={<Basic />} />
+        {/* 
+          중첩 라우팅 사용을 위해서 반드시 부모 Routes의 path 끝에 /*가 필수 
+        */}
+        <Route path='/route/*' element={<RoutePages />} />
+      </Routes>
     </>
   )
 }
