a572546ef75fce851ebfb569d1bc524a364e72be
ehdgn <ehdgn6570@gmail.com>
2025-11-05 16:41:57 +0900

    20251105 React Detail, Location, Navi

---
+++ f-iot-react/src/pages/b_route/E_NaviExample.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function E_NaviExample() {
+  const navigate = useNavigate();
+
+  const moveToDetail = () => {
+    // navigate의 두 번째 인자로 state 값 전달
+    navigate('/route/detail', {
+      state: {
+        userId: 10,
+        username: '곰세마리',
+        message: '리액트 라우트 훅 공부중'
+      }
+    });
+  }
+
+  return (
+    <div>
+      <h2>useNavigate 실습페이지</h2>
+      <button onClick={() => navigate(-1) }>뒤로 가기</button>
+      <button onClick={() => navigate('/route/locate') }>Location 페이지로 이동</button>
+      <button onClick={moveToDetail}>Detail 페이지로 이동 (+ state 전달)</button>
+    </div>
+  )
+}
+
+export default E_NaviExample
--- f-iot-react/src/pages/c_hooks/index.tsx
+++ f-iot-react/src/pages/c_hooks/index.tsx
@@ -22,6 +22,7 @@ import Custom01 from "./f_custom/Custom01";
 import Custom02 from "./f_custom/Custom02";
 import Custom03 from "./f_custom/Custom03";
 import TodoAppLocalStorage from "@/_practices/c_hooks/TodoAppLocalStorage";
+import Webcam from "./Webcam";
 
 function Index() {
   return (
@@ -29,6 +30,10 @@ function Index() {
       <h1 style={{ backgroundColor: "black", color: "white" }}>
         === 리액트 Hooks ===
       </h1>
+      <ToggleSection title="8. 리액트 Hooks - Webcam">
+        {/* <Webcam /> */}
+        <p></p>
+      </ToggleSection>
       <ToggleSection title="7. 리액트 Hooks Todo(LocalStorage)">
         <TodoAppLocalStorage />
       </ToggleSection>
